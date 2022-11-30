747f6f7976ddb4b9e26371ca49f973c8e88ea69e
lightlitebug <light.lite.bug@gmail.com>
2022-11-30 16:07:24 +0900

    ViewModel and StoreConnector, distinct, equlaity

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,10 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
+import '../redux/app_state.dart';
+import '../redux/counter/counter_action.dart';
 import 'increment_size.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,37 +14,72 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const IncrementSize(),
-            const SizedBox(height: 20.0),
-            Text(
-              'Increment Size:',
-              style: const TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 20.0),
-            Text(
-              'Counter:',
-              style: const TextStyle(
-                fontSize: 48,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text(
-                'Increment',
-                style: TextStyle(fontSize: 20.0),
-              ),
-            ),
-          ],
+        child: StoreConnector<AppState, _ViewModel>(
+          distinct: true,
+          converter: (Store<AppState> store) => _ViewModel.fromStore(store),
+          builder: (BuildContext context, _ViewModel vm) {
+            print('Building Column...');
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const IncrementSize(),
+                const SizedBox(height: 20.0),
+                Text(
+                  'Increment Size: ${vm.incrementSize}',
+                  style: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  'Counter: ${vm.counter}',
+                  style: const TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    vm.incrementCounter(vm.incrementSize);
+                  },
+                  child: const Text(
+                    'Increment',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
+    );
+  }
+}
+
+class _ViewModel extends Equatable {
+  final int incrementSize;
+  final int counter;
+  final void Function(int incrementSize) incrementCounter;
+  const _ViewModel({
+    required this.incrementSize,
+    required this.counter,
+    required this.incrementCounter,
+  });
+
+  @override
+  List<Object> get props => [incrementSize, counter];
+
+  static _ViewModel fromStore(Store<AppState> store) {
+    return _ViewModel(
+      incrementSize: store.state.incrementSizeState.incrementSize,
+      counter: store.state.counterState.counter,
+      incrementCounter: (int incrementSize) {
+        store.dispatch(
+          IncrementAction(incrementSize: incrementSize),
+        );
+      },
     );
   }
 }
--- lib/pages/increment_size.dart
+++ lib/pages/increment_size.dart
@@ -1,28 +1,60 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+import '../redux/app_state.dart';
+import '../redux/increment_size/increment_size_action.dart';
 
 class IncrementSize extends StatelessWidget {
-  const IncrementSize({Key? key}) : super(key: key);
+  const IncrementSize({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: TextField(
-        decoration: const InputDecoration(
-          labelText: 'Enter increment size',
-          border: InputBorder.none,
-          filled: true,
-        ),
-        onSubmitted: (String? value) {
-          if (value != null && value.trim().isNotEmpty) {
-            try {
-              final incrementSize = int.parse(value);
-            } on FormatException catch (e) {
-              print(e.toString());
-            }
-          }
+      child: StoreConnector<AppState, _ViewModel>(
+        distinct: true,
+        converter: (Store<AppState> store) => _ViewModel.fromStore(store),
+        builder: (BuildContext context, _ViewModel vm) {
+          print('Building TextField...');
+          return TextField(
+            decoration: const InputDecoration(
+              labelText: 'Enter increment size',
+              border: InputBorder.none,
+              filled: true,
+            ),
+            onSubmitted: (String? value) {
+              if (value != null && value.trim().isNotEmpty) {
+                try {
+                  final incrementSize = int.parse(value);
+                  vm.setIncrementSize(incrementSize);
+                } on FormatException catch (e) {
+                  print(e.toString());
+                }
+              }
+            },
+          );
         },
       ),
+    );
+  }
+}
+
+class _ViewModel extends Equatable {
+  final void Function(int incrementSize) setIncrementSize;
+  const _ViewModel({
+    required this.setIncrementSize,
+  });
+
+  @override
+  List<Object> get props => [];
+
+  static _ViewModel fromStore(Store<AppState> store) {
+    return _ViewModel(
+      setIncrementSize: (int incrementSize) {
+        store.dispatch(SetIncrementSizeAction(incrementSize: incrementSize));
+      },
     );
   }
 }
